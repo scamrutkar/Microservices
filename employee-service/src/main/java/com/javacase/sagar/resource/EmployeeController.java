8e8e995fb2a39ae014744ad755c238840565af6b
Sagar Amrutkar <scamrutkar@gmail.com>
2019-10-15 21:26:35 +0530

    adding employee-service implementation

--- employee-service/src/main/java/com/javacase/sagar/resource/EmployeeController.java
+++ employee-service/src/main/java/com/javacase/sagar/resource/EmployeeController.java
@@ -3,8 +3,8 @@ package com.javacase.sagar.resource;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.javacase.sagar.model.Employee;
@@ -21,7 +21,7 @@ public class EmployeeController {
 		}
 	};
 
-	@GetMapping(value = "/findEmployeeDetails/{employeeId}")
+	@RequestMapping(value = "/findEmployeeDetails/{employeeId}")
 	public Employee getEmployeeDetails(@PathVariable("employeeId") int id) {
 		return employeeData.get(id);
 	}
