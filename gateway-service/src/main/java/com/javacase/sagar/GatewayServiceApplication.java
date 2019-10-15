9a782e62814ee8236da4e5704e1f22fd00473e1d
Sagar Amrutkar <scamrutkar@gmail.com>
2019-10-15 21:58:15 +0530

    adding gateway-service and discovery-server

---
+++ gateway-service/src/main/java/com/javacase/sagar/GatewayServiceApplication.java
@@ -0,0 +1,20 @@
+package com.javacase.sagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
