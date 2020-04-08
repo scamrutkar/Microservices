59c037a8c405ac07a89740e9ed64fdf1fc789e91
Sagar Amrutkar <scamrutkar@gmail.com>
2020-04-08 08:47:34 +0530

    Adding db-service application class

---
+++ management-server/db-service/src/main/java/com/sagar/javacase/DbServiceApplication.java
@@ -0,0 +1,11 @@
+package com.sagar.javacase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DbServiceApplication.class,args);
+    }
+}
